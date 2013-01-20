dd4c255fd9170a35d849d45dfd4ae8c43bf136ab
jbrunton <john_brunton@hotmail.co.uk>
2013-01-20 14:50:59 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrancusiDemo::Application.config.secret_token = 'c91251c83bcc6e2bb398c4ce386b7375e1cabdba28db6dc6727ad7a5927d927938aaaefb169f3d06472ccb1d031aa1ef0cb2b51f903d460dac76b07b7184acb7'
